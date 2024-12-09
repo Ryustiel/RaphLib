b1d91fe3f439eeeb2701e95612d8102286871452
Raphael Nguyen <n.raphael285@gmail.com>
2024-12-09 09:27:14 +0100

    Updated export

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ A library to handle a LLM Conversation.
 import dotenv, langchain_core, pydantic_core, langchain, pydantic, langchain_openai  # Dependencies TODO : Manage package
 # Standard Library : os, typing, logging, asyncio
 
-from .prompts import ChatHistory
+from .prompts import ChatHistory, ChatMessage
 from .functions import LLMFunction, LLMFunctionResult, BatchLLMFunctionResult
 from .tools import LLMWithTools, ToolInterrupt
 from .helpers import balance_results, first_completed, escape_characters, LapTimer
@@ -13,6 +13,7 @@ from .setup import setup_env
 
 __all__ = [
     'ChatHistory', 
+    'ChatMessage',
     'LLMFunction', 
     'LLMFunctionResult', 
     'BatchLLMFunctionResult', 
